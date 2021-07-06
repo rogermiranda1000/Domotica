4d2392575acee08e5bead20f75da30524c317c54
rogermiranda1000 <rogermirandaperez@hotmail.com>
2021-07-06 11:42:52 +0200

    Library 1/2

--- libraries/DomoticConnector/src/DomoticConnector.cpp
+++ libraries/DomoticConnector/src/DomoticConnector.cpp
@@ -1,5 +1,58 @@
 #include "DomoticConnector.h"
 
-DomoticConnector::DomoticConnector() {
+char *_id;
+char *_group;
+WiFiClient _espClient;
+PubSubClient _client(_espClient);
+
+DomoticConnector::DomoticConnector(char *ip, short port, char *group, char *file_name) {
+	WiFi.mode(WIFI_STA);
+#if defined(WIFI_NAME) && defined(WIFI_PASS)
+	// credentials hardcoded
+	WiFi.begin(WIFI_NAME, WIFI_PASS);
+#else
+	// get credentials from SD
+	String ssid, password;
+	if (file_name != NULL && this->getDataFromSD(file_name, &ssid,&password)) WiFi.begin((const char*) ssid.c_str(), (const char*) password.c_str());
+#endif
+	this->_client.setServer(ip, port);
+
+	// copy group identifier
+	this->_group = (char*)malloc(sizeof(char)*(strlen(group)+1));
+	strcpy(this->_group, group);
+	this->_id = this->getID(group);
+}
+
+DomoticConnector::~DomoticConnector(void) {
+	free(this->_id);
+	free(this->_group);
+}
+
+char *DomoticConnector::getID(char *group) {
+	return (char*)malloc(sizeof(char)*10); // TODO
+}
+
+bool DomoticConnector::getDataFromSD(char *file_name, String *ssid, String *password) {
+	if (!SD.begin(SD_PIN)) return false;
 	
+	File myFile = SD.open(file_name);
+	if (myFile == NULL) return false;
+	
+	byte x = 0;
+	while (myFile.available()) {
+		String tmp = myFile.readStringUntil('\n');
+		if(x==0) *ssid = tmp;
+		else if(x==1) *password = tmp;
+
+		x++;
+	}
+	myFile.close();
+	return true;
+}
+
+void DomoticConnector::checkConnection() {
+  while (WiFi.status() != WL_CONNECTED);
+  while (!this->_client.connected()) {
+    if (this->_client.connect(this->_id)) this->_client.subscribe(this->_group);
+  }
 }
--- libraries/DomoticConnector/src/DomoticConnector.h
+++ libraries/DomoticConnector/src/DomoticConnector.h
@@ -2,18 +2,28 @@
 #define _DomoticConnector_h_
 
 #include "Arduino.h"
-#include <WiFiNINA.h>
-//#include <ESP8266WiFi.h>
+//#include <WiFiNINA.h>
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <SD.h>
+#include <string.h>
 
-#define SD_PIN D2
+#define SD_PIN D8
 
 class DomoticConnector {
   public:
-    DomoticConnector();
+    DomoticConnector(char *ip, short port, char *group, char *file_name);
+    DomoticConnector(char *ip, short port, char *group) : DomoticConnector(ip, port, group, NULL) {}
+	~DomoticConnector(void);
+	
   private:
-    byte _pin;
+    char *_id;
+	char *_group;
+	WiFiClient _espClient;
+	PubSubClient _client;
+	
+	char *getID(char *group);
+	bool getDataFromSD(String *ssid, String *password);
 };
 
 #endif
