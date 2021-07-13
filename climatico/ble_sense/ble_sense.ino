d99f08333cace15c00b9a38f24a4f7814eaccd73
roger.miranda <roger.miranda@students.salle.url.edu>
2021-07-14 01:46:22 +0200

    Guess what... arduino BLE sense is only for BLE... :(

--- climatico/ble_sense/ble_sense.ino
+++ climatico/ble_sense/ble_sense.ino
@@ -1,5 +1,7 @@
 #include <Arduino_HTS221.h>
 #include <Arduino_LPS22HB.h>
+#include <DomoticConnector.h>
+#include "secrets.h" // place here #define SECRET_SSID & SECRET_PASS
 
 #define DEBUG 1
 #if (DEBUG==1)
@@ -10,11 +12,30 @@
   #define DEBUG_PRINTLN(str)
 #endif
 
+#define MQTT_SERVER "192.168.1.79"
+#define MQTT_PORT   1883
+#define GROUP       "clim"
+
+const char *const WIFI_NAME = SECRET_SSID;
+const char *const WIFI_PASS = SECRET_PASS;
+
+DomoticConnector *connector;
+
 bool temp = true, pressure = true;
+
+volatile unsigned int retraso = 1000;
+unsigned long acumulado;
+
+void callback(char* topic, byte* payload, unsigned int length);
+void onReconnect(void);
+
 void setup() {
 #if (DEBUG == 1)
   Serial.begin(9600);
 #endif
+
+  Connector.setup(DEBUG, WIFI_NAME, WIFI_PASS);
+  connector = new DomoticConnector(MQTT_SERVER, MQTT_PORT, GROUP, onReconnect, ID_SUBSCRIPTION, callback);
 
   if (!HTS.begin()) {
     temp = false;
@@ -24,20 +45,44 @@ void setup() {
     pressure = false;
     DEBUG_PRINTLN("Failed to initialize pressure sensor!");
   }
+  
+  acumulado = millis();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  unsigned int value = 0;
+  for (int i=0;i<length;i++) value = value*10 + ((char)payload[i])-'0'; // atoi
+  
+  Serial.println("Retraso de " + String(value) + "s");
+  retraso = 1000*value;
+}
+
+void onReconnect(void) {
+  connector->publishSelf("central", "?");
 }
 
 void loop() {
+  connector->loop();
+  // el Arduino BLE no tiene EEPROM
+  
+  unsigned long current = millis();
+  if (current - acumulado < retraso) return;
+  acumulado = current;
+  DEBUG_PRINTLN();
+  
   if (temp) {
     float temperature = HTS.readTemperature();
     float humidity    = HTS.readHumidity();
     
     DEBUG_PRINT("Temperature = ");
     DEBUG_PRINT(temperature);
-    DEBUG_PRINTLN(" °C");
+    DEBUG_PRINTLN("C");
+    connector->publishSelf("central", "temperatura " + String(temperature));
   
     DEBUG_PRINT("Humidity    = ");
     DEBUG_PRINT(humidity);
-    DEBUG_PRINTLN(" %");
+    DEBUG_PRINTLN("%");
+    connector->publishSelf("central", "humedad " + String(humidity));
   }
 
   if (pressure) {
@@ -46,11 +91,6 @@ void loop() {
     DEBUG_PRINT("Pressure = ");
     DEBUG_PRINT(pressure);
     DEBUG_PRINTLN("hPa");
+    connector->publishSelf("central", "presion " + String(pressure));
   }
-
-  // print an empty line
-  DEBUG_PRINTLN();
-
-  // wait 1 second to print again
-  delay(1000);
 }
