eb0e50de775caf91b0219b8c9eae469f37716df1
roger.miranda <roger.miranda@students.salle.url.edu>
2021-07-14 01:21:18 +0200

    Feature: pressure with Arduino BLE Sense

--- climatico/ble_sense/ble_sense.ino
+++ climatico/ble_sense/ble_sense.ino
@@ -1,4 +1,5 @@
 #include <Arduino_HTS221.h>
+#include <Arduino_LPS22HB.h>
 
 #define DEBUG 1
 #if (DEBUG==1)
@@ -9,26 +10,43 @@
   #define DEBUG_PRINTLN(str)
 #endif
 
+bool temp = true, pressure = true;
 void setup() {
 #if (DEBUG == 1)
   Serial.begin(9600);
 #endif
 
-  if (!HTS.begin()) DEBUG_PRINTLN("Failed to initialize humidity temperature sensor!");
+  if (!HTS.begin()) {
+    temp = false;
+    DEBUG_PRINTLN("Failed to initialize humidity temperature sensor!");
+  }
+  if (!BARO.begin()) {
+    pressure = false;
+    DEBUG_PRINTLN("Failed to initialize pressure sensor!");
+  }
 }
 
 void loop() {
-  float temperature = HTS.readTemperature();
-  float humidity    = HTS.readHumidity();
+  if (temp) {
+    float temperature = HTS.readTemperature();
+    float humidity    = HTS.readHumidity();
+    
+    DEBUG_PRINT("Temperature = ");
+    DEBUG_PRINT(temperature);
+    DEBUG_PRINTLN(" °C");
+  
+    DEBUG_PRINT("Humidity    = ");
+    DEBUG_PRINT(humidity);
+    DEBUG_PRINTLN(" %");
+  }
 
-  // print each of the sensor values
-  DEBUG_PRINT("Temperature = ");
-  DEBUG_PRINT(temperature);
-  DEBUG_PRINTLN(" °C");
-
-  DEBUG_PRINT("Humidity    = ");
-  DEBUG_PRINT(humidity);
-  DEBUG_PRINTLN(" %");
+  if (pressure) {
+    float pressure = BARO.readPressure()*10; // pressure in kPa, but expecting hPa
+    
+    DEBUG_PRINT("Pressure = ");
+    DEBUG_PRINT(pressure);
+    DEBUG_PRINTLN("hPa");
+  }
 
   // print an empty line
   DEBUG_PRINTLN();
