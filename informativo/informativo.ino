059754537c2a91413647bd9706640fec85a47e49
roger.miranda <roger.miranda@students.salle.url.edu>
2021-07-19 17:41:25 +0200

    Feature: Modulo informativo send pressure

--- informativo/informativo.ino
+++ informativo/informativo.ino
@@ -24,6 +24,21 @@ const char *const WIFI_PASS = SECRET_PASS;
 MKRIoTCarrier carrier;
 DomoticConnector *connector;
 
+volatile unsigned int retraso = 1000;
+unsigned long acumulado;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  unsigned int value = 0;
+  for (int i=0;i<length;i++) value = value*10 + ((char)payload[i])-'0'; // atoi
+  
+  Serial.println("Retraso de " + String(value) + "s");
+  retraso = 1000*value;
+}
+
+void onReconnect(void) {
+  connector->publishSelf("central", "?");
+}
+
 void setup() {
 #if DEBUG
   Serial.begin(9600);
@@ -37,10 +52,23 @@ void setup() {
   }
 
   Connector.setup(DEBUG, WIFI_NAME, WIFI_PASS, SD_PIN, FILE_NAME);
-  connector = new DomoticConnector(MQTT_SERVER, MQTT_PORT, GROUP);
+  connector = new DomoticConnector(MQTT_SERVER, MQTT_PORT, GROUP, onReconnect, ID_SUBSCRIPTION, callback);
+  
+  acumulado = millis();
 }
 
 void loop() {
   connector->loop();
   // Arduino Wifi 1010 no tiene EEPROM
+
+  unsigned long current = millis();
+  if (current - acumulado < retraso) return;
+  acumulado = current;
+  DEBUG_PRINTLN();
+  
+  float pressure = carrier.Pressure.readPressure() * 10;
+  DEBUG_PRINT("Pressure = ");
+  DEBUG_PRINT(pressure);
+  DEBUG_PRINTLN(" hPa");
+  connector->publishSelf("central", "presion " + String(pressure));
 }
